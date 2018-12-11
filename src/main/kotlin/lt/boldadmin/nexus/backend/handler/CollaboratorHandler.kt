cd4652a37e326de26cc3994956a54011a750cf23
Okaminu <aurimasdgt@gmail.com>
2018-12-11 19:34:53 +0200

    BA-742: handlers for nexus-api services

---
+++ src/main/kotlin/lt/boldadmin/nexus/backend/handler/CollaboratorHandler.kt
@@ -0,0 +1,46 @@
+package lt.boldadmin.nexus.backend.handler
+
+import lt.boldadmin.nexus.api.service.CollaboratorService
+import lt.boldadmin.nexus.api.type.entity.Collaborator
+import org.springframework.web.reactive.function.server.ServerRequest
+import org.springframework.web.reactive.function.server.ServerResponse
+import org.springframework.web.reactive.function.server.ServerResponse.ok
+import org.springframework.web.reactive.function.server.body
+import org.springframework.web.reactive.function.server.bodyToMono
+import reactor.core.publisher.Mono
+
+open class CollaboratorHandler(
+    private val collaboratorService: CollaboratorService
+) {
+
+    open fun existsById(req: ServerRequest): Mono<ServerResponse> =
+        ok().body(Mono.just(collaboratorService.existsById(req.pathVariable("collaboratorId"))))
+
+    open fun existsByMobileNumber(req: ServerRequest): Mono<ServerResponse> =
+        ok().body(Mono.just(collaboratorService.existsByMobileNumber(req.pathVariable("mobileNumber"))))
+
+    open fun save(req: ServerRequest): Mono<ServerResponse> =
+        req.bodyToMono<Collaborator>()
+            .doOnNext { collaboratorService.save(it) }
+            .flatMap { ok().build() }
+
+    open fun createWithDefaults(req: ServerRequest): Mono<ServerResponse> =
+        ok().body(Mono.just(collaboratorService.createWithDefaults()))
+
+    open fun getById(req: ServerRequest): Mono<ServerResponse> =
+        ok().body(Mono.just(collaboratorService.getById(req.pathVariable("collaboratorId"))))
+
+    open fun getByMobileNumber(req: ServerRequest): Mono<ServerResponse> =
+        ok().body(Mono.just(collaboratorService.getByMobileNumber(req.pathVariable("mobileNumber"))))
+
+    open fun update(req: ServerRequest): Mono<ServerResponse> =
+        req.bodyToMono<String>()
+            .doOnNext { collaboratorService.update(req.pathVariable("collaboratorId"), req.pathVariable("attributeName"), it) }
+            .flatMap { ok().build() }
+
+    open fun updateOrderNumber(req: ServerRequest): Mono<ServerResponse> =
+        req.bodyToMono<Short>()
+            .doOnNext { collaboratorService.updateOrderNumber(req.pathVariable("collaboratorId"), it) }
+            .flatMap { ok().build() }
+
+}
