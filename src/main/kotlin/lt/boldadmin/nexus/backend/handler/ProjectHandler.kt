bf8d094c1184f63db36af3ddf8a0e80a7e591b8d
vsh1ft <kastytis.ch@gmail.com>
2019-04-04 22:07:34 +0300

    BA-502: location update backend endpoint

--- src/main/kotlin/lt/boldadmin/nexus/backend/handler/ProjectHandler.kt
+++ src/main/kotlin/lt/boldadmin/nexus/backend/handler/ProjectHandler.kt
@@ -1,6 +1,7 @@
 package lt.boldadmin.nexus.backend.handler
 
 import lt.boldadmin.nexus.api.service.ProjectService
+import lt.boldadmin.nexus.api.type.valueobject.Location
 import org.springframework.web.reactive.function.server.*
 import org.springframework.web.reactive.function.server.ServerResponse.ok
 import reactor.core.publisher.Mono
@@ -24,5 +25,11 @@ open class ProjectHandler(private val projectService: ProjectService) {
             .map { it.toShort() }
             .doOnNext { projectService.updateOrderNumber(req.pathVariable("projectId"), it) }
             .flatMap { ok().build() }
+
+    open fun updateLocation(req: ServerRequest): Mono<ServerResponse> {
+        return req.bodyToMono<Location>()
+            .doOnNext { projectService.updateLocation(req.pathVariable("projectId"), it) }
+            .flatMap { ok().build() }
+    }
 
 }
